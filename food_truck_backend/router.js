778f66bee75f32cdaf126cf61331b7a22d1c90f7
kushkapadia <kush.salesbrahma@gmail.com>
2024-02-17 20:23:56 +0530

    Customer register done

--- food_truck_backend/router.js
+++ food_truck_backend/router.js
@@ -3,12 +3,14 @@ const router = express.Router()
 
 const foodTruckController = require('./controllers/foodTruckController')
 const orderController = require('./controllers/orderController.js')
+const customerController = require('./controllers/customerController.js')
 //Admin related apps
 router.get('/', foodTruckController.home)
 
 //Cusatomer related Routes (API)
 router.post('/place-order', orderController.placeOrder)
 router.post('/complete-order', )
+router.post('/register-customer', customerController.registerCustomer)
 
 
 // 404
